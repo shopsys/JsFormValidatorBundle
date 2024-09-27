0dfacb6076cc150fe8439b5dae05e1d4bc4fbcf4
Rostislav Vitek <rostislav.vitek@shopsys.com>
2024-09-27 17:34:42 +0200

    remove dependency on security bundle, only security-csrf is needed

--- Tests/app/config/bundles.php
+++ Tests/app/config/bundles.php
@@ -2,7 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Fp\JsFormValidatorBundle\FpJsFormValidatorBundle::class => ['all' => true],
